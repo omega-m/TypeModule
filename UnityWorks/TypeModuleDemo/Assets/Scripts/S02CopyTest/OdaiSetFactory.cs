7c87d85861262b40b0d7a983421e64f6e19ee010
おめが <typenotes.official@gmail.com>
2022-02-17 16:31:04 +0900

    デモシーン COPY続き

---
+++ UnityWorks/TypeModuleDemo/Assets/Scripts/S02CopyTest/OdaiSetFactory.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 関数ポインタ作成用
+/// </summary>
+public delegate List<string> OdayFuncInner();
+public struct OdayFunction {
+    public OdayFunction(OdayFuncInner aFunc) {
+        m_func = aFunc;
+    }
+    public OdayFuncInner m_func;
+}
+
+/// <summary>02CopyTest用の、タイピングする文章セットのファクトリ</summary>
+static public class OdaiSetFactory{
+
+    #region 静的メソッド
+    /// <summary>
+    /// <para>02CopyTest用の、タイピングする文章セットを作成し返却します。</para>
+    /// <para>呼び出す度に、ランダムな文章セットが返却されます。</para>
+    /// </summary>
+    /// <returns></returns>
+    static public List<string> Create() {
+        OdayFunction[] odaiList = {
+            new OdayFunction(CreatePhony),
+            new OdayFunction(CreateGoodbyeDeclaration),
+            new OdayFunction(CreateVampire),
+        };
+
+        int idx = (int)Mathf.Floor(Random.value * odaiList.Length);
+        return odaiList[idx].m_func();
+    }
+    #endregion
+
+    #region 内部メソッド
+    /// <summary>フォニイ</summary>
+    /// <returns>「フォニイ」の歌詞セット</returns>
+    static private List<string> CreatePhony() {
+            return new List<string>() {
+            "このよでぞうかよりきれいなはなはないわ",
+            "なぜならばすべてはうそでできている",
+            "antipathy world",
+            "ぜつぼうのあめはあたしのかさをついて",
+            "しめらすまえがみとこころのりめん",
+            "わずらわしいわ",
+            "いつしかことのははとうにかれきって",
+            "ことのみがあたしにうれている",
+            "かがみにうつりうそをえがいて",
+            "みずからをみうしなっためいく",
+            "パパッパラパッパララッパッパ",
+            "なぞなぞかぞえてあそびましょう",
+            "タタッタラタッタララッタッタ",
+            "なぜなぜここでおどっているでしょう",
+            "かんたんなこともわからないわ",
+            "あたしってなんだっけ",
+            "それすらよるのてにほだされて",
+            "あいのようにきえる",
+            "さようならもいえぬままないたフォニイ",
+            "うそにからまっているあたしはフォニイ",
+            "antipathy world",
+        };
+    }
+
+    /// <summary>「グッバイ宣言」</summary>
+    /// <returns>「グッバイ宣言」の歌詞セット</returns>
+    static private List<string> CreateGoodbyeDeclaration() {
+        return new List<string>() {
+            "エマージェンシーれいじやつらは",
+            "クレイジーインザタウンうちにこもって",
+            "ゴロゴロゴロゴロと",
+            "だらくのよるにからみついた",
+            "ルルルはなつことばは",
+            "ルルルくさっていた",
+            "せいろんもじょうしきも",
+            "いみをもたないとかいにサヨウナラ",
+            "ひきこもりぜったいジャスティス",
+            "おれのわたしだけのおりのなかで",
+            "ききころしてランデブー",
+            "おれのわたしのねがきみにそまるまで",
+            "ひきこもりぜったいジャスティス",
+            "おれのわたしだけのおりのなかで",
+            "ききころしてランデブー",
+            "おれのわたしのねをきみがつつむだけ",
+            "wowow ときがきたいま",
+            "wowow エゴはなつのさ",
+            "wowow うちにこもってくるいざく",
+        };
+    }
+
+    /// <summary>「ヴァンパイア」</summary>
+    /// <returns>「ヴァンパイア」の歌詞セット</returns>
+    static private List<string> CreateVampire() {
+        return new List<string>() {
+            "あたしヴァンパイア",
+            "いいの？すっちゃっていいの？",
+            "「もうむりもうむり」なんてわるいこだね",
+            "ためしたいないっぱいではきたい",
+            "まだぜったいいけるよ",
+            "さいていさいこうずっといききしてる",
+            "あまくなるふあんのかじつ",
+            "No more はってんしっといきをしても",
+            "いらないだけうるさいだけ",
+            "だれかといればそれはたられば",
+            "つよがってたってきもちにゃさからえない",
+            "はなれていてもかんじてるエモ",
+            "つながってたしかめたらしねるかも",
+            "いいもんかなしいもんせつないもん",
+            "きみのすべてをくらうまでぜっきょう",
+            "あたしヴァンパイア",
+            "いいの？すっちゃっていいの？",
+            "「もうむりもうむり」なんてわるいこだね",
+            "ためしたいないっぱいではきたい",
+            "まだぜったいいけるよ",
+            "あたしヴァンパイア",
+            "もとめちゃってまたからしちゃってほらやなかんじ",
+            "ないてわすれたら「はじめまして」",
+            "あたしヴァンパイア",
+            "あいじょうをくださいまだぜったいいけるよ",
+            "あたしヴァンパイアまずはこっちおいで",
+        };
+    }
+
+    #endregion
+}
--- UnityWorks/TypeModuleDemo/Assets/Scripts/S02CopyTest/S02Manager.cs
+++ UnityWorks/TypeModuleDemo/Assets/Scripts/S02CopyTest/S02Manager.cs
@@ -15,7 +15,7 @@ public class S02Manager : MonoBehaviour {
     /// <summary>ゲームの状態を定義</summary>
     public enum STATE {
         NONE,
-        /// <summary>スタートボタンが表示している画面</summary>
+        /// <summary>スタートボタンを表示している画面</summary>
         MENU,
         /// <summary>カウントダウン中</summary>
         COUNTDOWN,
@@ -31,12 +31,16 @@ public class S02Manager : MonoBehaviour {
     void Start() {
         SetupTypeModule();
 
+        m_audioSource = GetComponent<AudioSource>();
+        m_timer = GetComponent<Timer>();
+        m_gameParams.m_stopwatch = GetComponent<Stopwatch>();
+
         m_canvasMenu.enabled = false;
         m_canvasCountdown.enabled = false;
+        m_canvasGame.enabled = false;
 
         ChangeState(STATE.MENU);
     }
-
 
     void Update() {
         switch (m_state) {
@@ -60,7 +64,6 @@ public class S02Manager : MonoBehaviour {
                 m_step++;
                 break;
             case 1:
-                //none
                 break;
         }
     }
@@ -90,13 +93,12 @@ public class S02Manager : MonoBehaviour {
     }
 
     /// <summary>ゲームスタートボタンが押された時に呼び出されます</summary>
-    public void OnClickGameStart() {
+    public void OnClickStart() {
         if (m_state != STATE.MENU) { return; }
         m_canvasMenu.enabled = false;
         InitGame();
         ChangeState(STATE.COUNTDOWN);
     }
-
     #endregion
 
 
@@ -105,35 +107,127 @@ public class S02Manager : MonoBehaviour {
     private void UpdateCountDown() {
         switch (m_step) {
             case 0:
-                m_textCountDown.text = "3";
-                m_timer = new System.Timers.Timer(3000 - 1);
-                m_timer.AutoReset = false;
-                m_timer.Enabled = true;
-                m_timer.Elapsed += OnTimerTick;
-                m_timer.Start();
                 m_canvasCountdown.enabled = true;
+                m_textCountDown.text = "3";
+                m_timer.StartTimer(3.0f - 0.001f);   //UIが一時だけ4になるのを防ぐ為
+                m_audioSource.PlayOneShot(m_audioCountDown);
                 m_step++;
                 break;
+            case 1:
+                if (m_timer.IsCompleted) {
+                    m_step++;
+                } else {
+                    int nowTime = (int)Math.Ceiling(m_timer.RemainTime);
+                    m_textCountDown.text = nowTime.ToString();
+                }
+                break;
+            case 2:
+                m_canvasCountdown.enabled = false;
+                ChangeState(STATE.GAME);
+                break;
         }
-    }
-
-    /// <summary>【COUNTDOWN】タイマー更新イベント</summary>
-
-    private void OnTimerTick(System.Object aSource, ElapsedEventArgs e) {
-        m_textCountDown.text = e.SignalTime.Second.ToString();
     }
     #endregion
 
 
     #region 【GAME】メソッド
-    /// <summary>【GAME】更新処理</summary>
+    /// <summary>【GAME】更新処理(【MENU】の開始ボタンが押された時に呼び出します)</summary>
     private void InitGame() {
-        
+        var p = m_gameParams;
+        p.Clear();
+        p.m_odaiSet = OdaiSetFactory.Create();
+        p.m_stopwatch = GetComponent<Stopwatch>();
+        p.m_stopwatch.ResetStopwatch();
     }
 
     /// <summary>【GAME】更新処理</summary>
     private void UpdateGame() {
-        Debug.Log("GAME");
+        //Debug.Log("GAME");
+
+        var p = m_gameParams;
+
+        switch (m_step) {
+            case 0:
+                m_canvasGame.enabled = true;
+                p.m_stopwatch.StartStopwatch();
+                m_tp.TargetStr = p.m_odaiSet[p.m_odaiIdx];
+                m_tp.IsRun = true;
+                m_step++;
+                break;
+            case 1:
+                break;
+        }
+    }
+
+    /// <summary>【GAME】文字列が新たにセットされた時のイベント</summary>
+    /// <param name="res">入力の状態</param>
+    private void TMOnSetup(CopyInputCheckerResults res) {
+        //Debug.Log("TMOnSetup");
+        SetInputText(true);
+    }
+
+    /// <summary>【GAME】キーが打たれた時のイベント</summary>
+    /// <param name="res">入力の状態</param>
+    private void TMOnInput(CopyInputCheckerResults res) {
+        //Debug.Log("TMOnInput");
+    }
+
+    /// <summary>【GAME】キーが正しく打たれた時のイベント</summary>
+    /// <param name="res">入力の状態</param>
+    private void TMOnCorrect(CopyInputCheckerResults res) {
+        //Debug.Log("TMOnCorrect");
+        var p = m_gameParams;
+        p.m_totalCorrectKey++;
+        SetInputText(true);
+        m_audioSource.PlayOneShot(m_audioCorrect);
+    }
+
+    /// <summary>【GAME】ミスタイプされた時のイベント</summary>
+    /// <param name="res">入力の状態</param>
+    private void TMOnMiss(CopyInputCheckerResults res) {
+        //Debug.Log("TMOnMiss");
+        var p = m_gameParams;
+        p.m_totalMissKey++;
+        SetInputText(false);
+        m_audioSource.PlayOneShot(m_audioMiss);
+    }
+
+    /// <summary>【GAME】文字列を打ち切った時のイベント</summary>
+    /// <param name="res">入力の状態</param>
+    private void TMOnComplete(CopyInputCheckerResults res) {
+        //Debug.Log("TMOnComplete");
+        var p = m_gameParams;
+        p.m_odaiIdx++;
+        if (p.m_odaiIdx >= p.m_odaiSet.Count) {
+            m_tp.IsRun = false;
+            p.m_stopwatch.PauseStopwatch();
+            ChangeState(STATE.RESULT);
+        } else {
+            m_tp.TargetStr = p.m_odaiSet[p.m_odaiIdx];
+        }
+    }
+
+    /// <summary>【GAME】現在の状態の入力文字列をセット</summary>
+    /// <param name="aIsCorrect">前回の入力が正しく入力されたかどうか</param>
+    private void SetInputText(bool aIsCorrect) {
+        string tmp = "";
+        tmp = (BASE_COLOR + m_tp.StrDone + COLOR_END);
+        if (aIsCorrect) {
+            tmp += (CURRENT_COLOR + m_tp.StrCurrent + COLOR_END);
+        } else {
+            tmp += (MISS_COLOR + m_tp.StrCurrent + COLOR_END);
+        }
+        tmp += (BASE_COLOR + m_tp.StrYet + COLOR_END);
+        m_textInput.text = tmp;
+
+        tmp = (BASE_COLOR + m_tp.StrDoneRaw + COLOR_END);
+        if (aIsCorrect) {
+            tmp += (CURRENT_COLOR + m_tp.StrCurrentRaw + COLOR_END);
+        } else {
+            tmp += (MISS_COLOR + m_tp.StrCurrentRaw + COLOR_END);
+        }
+        tmp += (BASE_COLOR + m_tp.StrYetRaw + COLOR_END);
+        m_textInputRaw.text = tmp;
     }
     #endregion
 
@@ -147,13 +241,16 @@ public class S02Manager : MonoBehaviour {
 
 
     #region 内部メソッド
-
     /// <summary>TypeModuleの初期化処理</summary>
     void SetupTypeModule() {
         m_tp = GetComponent<TypeModule>();
         m_tp.Mode = TypeModule.MODE.COPY;
         m_tp.IsRun = false;
-        //m_tp.AddEventListenerOnChange(TMOnChange);
+        m_tp.AddEventListenerOnSetup(TMOnSetup);
+        m_tp.AddEventListenerOnInput(TMOnInput);
+        m_tp.AddEventListenerOnCorrect(TMOnCorrect);
+        m_tp.AddEventListenerOnMiss(TMOnMiss);
+        m_tp.AddEventListenerOnComplete(TMOnComplete);
         OnChangeedKeyboardLayout();
     }
 
@@ -172,17 +269,82 @@ public class S02Manager : MonoBehaviour {
     [Space(10)]
     public Canvas m_canvasMenu;
     public Canvas m_canvasCountdown;
+    public Canvas m_canvasGame;
 
     [Space(10)]
     public Text m_textCountDown;
+    public AudioClip m_audioCountDown;
+
+    [Space(10)]
+    public Text m_textInput;
+    public Text m_textInputRaw;
+
+    public AudioClip m_audioCorrect;
+    public AudioClip m_audioMiss;
     #endregion
 
 
     #region メンバ
     private TypeModule m_tp;
+    private AudioSource m_audioSource;
+
     private STATE m_state = STATE.MENU;
     private int m_step = 0;
 
-    private System.Timers.Timer m_timer;
+    private Timer m_timer;
+    private GameParams m_gameParams = new GameParams();
     #endregion 
+
+
+    #region 定数
+    static private readonly string BASE_COLOR =     "<color=#A19A9A>";
+    static private readonly string MISS_COLOR =     "<color=#FF5145>";
+    static private readonly string CURRENT_COLOR =  "<color=#FFFFFF>";
+    static private readonly string COLOR_END =      "</color>";
+    #endregion
+}
+
+
+
+/// <summary>ゲーム時用のパラメータ</summary>
+public class GameParams {
+
+
+    #region メソッド
+    /// <summary>内部データをクリア</summary>
+    public void Clear() {
+        m_odaiSet = new List<string>();
+        m_odaiIdx = 0;
+        m_totalCorrectKey = 0;
+        m_totalMissKey = 0;
+        m_stopwatch.ResetStopwatch();
+    }
+    #endregion
+
+
+    #region プロパティ
+    /// <summary>正確率(0-100で返却)</summary>
+    public float  AccuracyRate {
+        get { return (float)m_totalCorrectKey / (float)(m_totalCorrectKey + m_totalMissKey) * 100; }
+    }
+
+    /// <summary>key/sec</summary>
+    public float KeySec {
+        get { return m_totalCorrectKey / m_stopwatch.Time; }
+    }
+
+    #endregion
+
+    #region メンバ
+    /// <summary>打つ対象の文字列セット</summary>
+    public List<string>     m_odaiSet;
+    /// <summary>現在打っている文字列のidx</summary>
+    public int              m_odaiIdx;
+    /// <summary>ストップウォッチ、時間測定用</summary>
+    public Stopwatch        m_stopwatch;
+    /// <summary>正しく打てた数</summary>
+    public int              m_totalCorrectKey;
+    /// <summary>ミスタイプした数</summary>
+    public int              m_totalMissKey;
+    #endregion
 }
